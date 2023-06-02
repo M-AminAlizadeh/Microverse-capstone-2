683162848e0a1c68a400b512a7acf167ae50e05d
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-06-02 14:41:30 +0330

    Build comment section structure in comment popup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import displayItems from './modules/displayItems.js';
 const baseURL = 'https://api.tvmaze.com/shows/1/episodes';
 const involvementURL =
   'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UKP27MmenkdUVvm9H93H/';
-
 // Display items
 displayItems(baseURL);
-
 
 // Comment Popup
 const modalTitle = document.querySelector('.modal-title');
---
+++ src/modules/comment.js
@@ -0,0 +1,7 @@
+const involvementURL =
+  'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UKP27MmenkdUVvm9H93H/';
+
+
+const comment = ()=>{
+  
+}
