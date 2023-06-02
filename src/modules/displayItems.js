295d3f5ae78be1363c7d67c3684edd18449e1f6b
Gergito Kusse <gergito2@gmail.com>
2023-06-02 17:38:53 +0300

    update dispal

--- src/modules/displayItems.js
+++ src/modules/displayItems.js
@@ -5,81 +5,79 @@ const involvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/c
 const mainContent = document.querySelector('main');
 
 // Showing Items
-const displayItems = async (baseURL, items, AllLikes) => {
-  const res = await fetch(baseURL);
-  const data = await res.json();
+const displayItems = async (baseURL, item, allLikes) => {
   mainContent.innerHTML = '';
   let content = '';
-  items.forEach((item, index) => {
-    if (index < 29) {
+  // items.forEach((item, index) => {
+  for (let i = 0; i < 32; i++) {
+    const mealLikes = allLikes.find((like) => like.item_id === (item[i].id).toString());
+    if (i < 29) {
       content += `
       <!-- Card -->
-        <div class="card" style="width: 20rem" id="${item.id}">
+        <div class="card" style="width: 20rem" id="${item[i].id}">
           <!-- Snapshoot -->
           <img
-            src="${item.image.original}"
+            src="${item[i].image.original}"
             class="card-img-top"
-            alt="${item.name} poster" />
+            alt="${item[i].name} poster" />
           <!-- Name and Like row -->
           <div
             class="card-body d-flex justify-content-between align-items-center">
-            <h4 class="card-title text-capitalize"> ${item.name} </h4>
+            <h4 class="card-title text-capitalize"> ${item[i].name} </h4>
             <!-- unlike icon-->
             <div class="like-container d-flex flex-column align-items-center">
                 <img
                   width="30"
                   height="30"
-                  id="${item.id}"
+                  id="${item[i].id}"
                   src="https://img.icons8.com/ios/50/like--v1.png"
                   alt="unlike icon" 
                   class="like-btn" />
-              <p class="card-text"><span class="me-2" id='likes-${
-    item.id
-  }'></span>Likes</p>
+              <span class="me-2">${mealLikes ? mealLikes.likes : 0}</span>Likes
               <!-- src="https://img.icons8.com/ios-filled/50/FA5252/like--v1.png" -->
             </div>
           </div>
           <!-- Summary -->
           <div class="card-body my-0 py-0">
-            ${item.summary.slice(0, 100)}...
+            ${item[i].summary.slice(0, 100)}...
           </div>
           <!-- Buttons -->
           <div class="card-body d-flex justify-content-between">
             <button type="button" class="btn btn-info text-light comment-btn" data-bs-toggle="modal" width="2rem" height="2rem" id="${
-    item.id
+    item[i].id
   }" data-bs-target="#e">Comments</button>
             <button type="button" class="btn btn-secondary" id="${
-    item.id
+    item[i].id
   }">Reservation</button>
           </div>
         </div>
       `;
     }
-  });
+  }
   mainContent.innerHTML = content;
-  return data;
+  return item;
 };
 
 // Showing likes count
-const showLikesCount = async (invoUrl) => {
-  const likesData = await getLike(invoUrl);
-  likesData.forEach((eachItemData) => {
-    const likesCount = [`${eachItemData.likes}`];
-    const likesCountElement = document.getElementById(
-      `likes-${eachItemData.item_id}`,
-    );
-    if (likesCountElement) {
-      likesCountElement.innerHTML = likesCount;
-    }
-  });
-};
-showLikesCount(involvementURL);
-// Send likes to server
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('like-btn')) {
-    postLike(involvementURL, e.target.id);
-    showLikesCount();
-  }
-});
+// const showLikesCount = async (invoUrl) => {
+//   const likesData = await getLike(invoUrl);
+//   likesData.forEach((eachItemData) => {
+//     const likesCount = [`${eachItemData.likes}`];
+//     const likesCountElement = document.getElementById(
+//       `likes-${eachItemData.item_id}`,
+//     );
+//     if (likesCountElement) {
+//       likesCountElement.innerHTML = likesCount;
+//     }
+//   });
+// };
+// showLikesCount(involvementURL);
+// // Send likes to server
+// document.addEventListener('click', (e) => {
+//   if (e.target.classList.contains('like-btn')) {
+//     postLike(involvementURL, e.target.id);
+//     showLikesCount();
+//   }
+// });
 
 export default displayItems;
--- src/modules/like.js
+++ src/modules/like.js
@@ -1,3 +1,5 @@
+const involvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UKP27MmenkdUVvm9H93H/';
+
 const postLike = async (invoUrl, itemId) => {
   const res = await fetch(`${invoUrl}likes`, {
     method: 'POST',
@@ -24,5 +26,38 @@ const allItems = async (baseURL) => {
   return data;
 };
 
+export const updateLikesOnDOM = (likeBtn) => {
+  const likeElement = likeBtn.nextElementSibling;
+  const likes = Number(likeElement.innerHTML.split(' ')[0]);
+  likeElement.innerHTML = `${likes + 1} likes`;
+};
+
+export const addLike = (likeBtn) => {
+  fetch(involvementURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: likeBtn.id,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    });
+};
+
+export const addLikesListenerButtons = () => {
+  const Buttons = document.querySelectorAll('.like-btn');
+  Buttons.forEach((eachlike) => {
+    eachlike.addEventListener('click', (e) => {
+      e.preventDefault();
+      addLike(eachlike);
+      updateLikesOnDOM(eachlike);
+    });
+  });
+};
 
 export { postLike, getLike, allItems };
