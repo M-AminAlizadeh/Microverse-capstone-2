99efe09d06b3f017d9ca76bb5f6100ba89441bd5
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-06-02 15:27:35 +0330

    Get comments of each item from api

--- src/modules/comment.js
+++ src/modules/comment.js
@@ -1,7 +1,13 @@
 const involvementURL =
   'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/UKP27MmenkdUVvm9H93H/';
 
+const comment = () => {
+};
+// Show comments
+const getComments = async (invoLink, id) => {
+  const res = await fetch(`${invoLink}comments?item_id${id}`);
+  const data = await res.json();
+  return data;
+};
 
-const comment = ()=>{
-  
-}
+export default getComments;
